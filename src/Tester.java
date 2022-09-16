6c773b4122d559e8d4aab6fde9becf722a1b77bb
JackBurghardt <jackburghardt@yahoo.com>
2022-09-16 10:56:02 -0700

    Tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,30 +1,16 @@
+
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.NoSuchAlgorithmException;
 
 public class Tester {
-<<<<<<< HEAD
-public static void main (String []args) throws IOException, NoSuchAlgorithmException  {
-=======
-public static void main (String []args) throws IOException  {
->>>>>>> main
-//	String a = "Object";
-//	Blob blobby;
-//	try {
-//		blobby = new Blob (a);
-//	} catch (IOException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	}
-//	System.out.println( Blob.encryptThisString(a));
-<<<<<<< HEAD
-	Blob blobby = new Blob ("Tester.txt");
-	blobby.newFile();
-=======
-	Blob blobby = new Blob ("s");
->>>>>>> main
-}
+
+	public static void main(String[] args) throws IOException {
+		Blob b = new Blob ("/Users/meeraburghardt/eclipse-workspace/Git Prereq/something.txt");
+	System.out.println(b.encryptThisString("abcdefg"));
+		Index i = new Index();
+		i.init();
+		i.addBlob("/Users/meeraburghardt/eclipse-workspace/Git Prereq/something.txt");
+		i.removeBlob("/Users/meeraburghardt/eclipse-workspace/Git Prereq/something.txt");
+	}
+
 }
